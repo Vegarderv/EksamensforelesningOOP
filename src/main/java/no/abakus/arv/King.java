2f99896a23a26d6b3ec2fe46f9f17323c7540c17
Vegard Ervik <vegarderv@gmail.com>
2022-06-01 19:08:26 +0200

    arv

---
+++ src/main/java/no/abakus/arv/King.java
@@ -0,0 +1,22 @@
+package no.abakus.arv;
+
+public  class King extends Brikke {
+
+    public King(int x, int y) {
+        super(x, y);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean isValidMove(int x, int y) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void move(int x, int y) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
