eacf8b37220681a30b483e859bf60e193f8da59c
Vegard Ervik <vegarderv@gmail.com>
2022-06-01 17:32:09 +0200

    iteration

---
+++ src/main/java/no/abakus/iteration/StringMergingIterator.java
@@ -0,0 +1,28 @@
+package no.abakus.iteration;
+
+import java.text.StringCharacterIterator;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class StringMergingIterator implements Iterator<String> {
+
+    private Iterator<String> i1, i2;
+
+    public StringMergingIterator(String s1, String s2) {
+        this.i1 = Arrays.asList(s1.split("")).iterator();
+        this.i2 = Arrays.asList(s2.split("")).iterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return i1.hasNext() || i2.hasNext();
+    }
+
+    @Override
+    public String next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+}
