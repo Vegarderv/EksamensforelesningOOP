47e4775a1d2bffc01188fa88bee65487591cc00b
Vegard Ervik <vegarderv@gmail.com>
2022-06-02 14:10:48 +0200

    forelesning

--- pensum/src/main/java/no/abakus/observable/IHaveValues.java
+++ pensum/src/main/java/no/abakus/observable/IHaveValues.java
@@ -2,4 +2,11 @@ package no.abakus.observable;
 
 public class IHaveValues {
     
+    private final ObservableString string1;
+    private final ObservableString string2;
+    private final ObservableString string3;
+
+    public IHaveValues() {
+        
+    }
 }
